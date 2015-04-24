9d977954e14060dc55bfd896e359f86dd9abcaba
Jon Rohan <rohan@github.com>
2015-04-23 22:35:49 -0400

    Built jonrohan.codes from commit df2bb06 on branch master

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,7 +21,6 @@
     emd = ["j", "on", "@", "jonr", "ohan", ".me"];
     $('.js-em').attr("href", "mailto:" + (emd.join("")));
     if ($(".not-found-page").length) {
-      console.log("url(\"" + window.gifs[Math.floor(Math.random() * (window.gifs.length + 1))].location + "\")");
       return $(".not-found-page").css('background-image', "url(\"" + window.gifs[Math.floor(Math.random() * (window.gifs.length + 1))].location + "\")");
     }
   });
